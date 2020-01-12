1c0b5f4c1f94208225fd12c3835e588f63259ac3
rtlessly <tlessly@hotmail.com>
2020-01-12 08:31:47 -0600

    Converted to a Task

--- RTL_Blinker.cpp
+++ RTL_Blinker.cpp
@@ -7,7 +7,6 @@
 #define DEBUG 0
  
 #include <Arduino.h>
-#include <Streaming.h>
 #include <RTL_Stdlib.h>
 #include "RTL_Blinker.h"
 
@@ -22,11 +21,53 @@ Blinker::Blinker(uint8_t ledPin, uint16_t period, uint16_t dutyCycle)
 {
     _state.pin = ledPin;
     _state.mode = REPEAT_MODE;
-    _state.running = false;
     _state.led = 0;
     
     pinMode(_state.pin, OUTPUT);
     SetBlinkRate(period, dutyCycle);
+}
+
+
+void Blinker::StateChanging(TaskState newState)
+{
+    switch (newState)
+    {
+        case TaskState::Resuming:
+            On();   // Aways start with LED on
+            _timeout = millis() + _onTime;
+            TRACE(Logger(_classname_, F("Resuming")) << F(": LED=") << _state.led << F(": Mode=") << _state.mode << endl);
+            break;
+
+        case Suspending:
+            Off();  // Always turn the LED off
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+void Blinker::Poll()
+{
+    TRACE(Logger(_classname_, F("Poll")) << endl);
+
+    auto now = millis();
+
+    if (now >= _timeout)
+    {
+        if (_state.mode == ONE_SHOT_MODE)
+        {
+            Suspend();
+        }
+        else
+        {
+            Toggle();
+            _timeout = now + (_state.led ? _onTime : _offTime);
+        }
+
+        TRACE(Logger(_classname_, F("Poll")) << F(": LED=") << _state.led << endl);
+    }
 }
 
 
@@ -42,21 +83,8 @@ void Blinker::SetBlinkRate(uint16_t period, uint16_t dutyCycle)
 }
 
 
-void Blinker::Start()
-{
-    if (_state.running) return; // Ignore if already running
-
-    _state.running = true;
-    _startTime = millis();
-    On();   // Aways start with LED on
-
-    TRACE(Logger(_classname_, F("Start")) << F(": LED=") << _state.led << F(": Mode=") << _state.mode << endl);
-}
-
-
 void Blinker::Start(uint16_t period, uint16_t dutyCycle)
 {
-    _state.running = false;      // Turn off first to allow restarting in new mode
     _state.mode = REPEAT_MODE;
     SetBlinkRate(period, dutyCycle);
     Start();
@@ -67,27 +95,7 @@ void Blinker::OneShot(uint16_t period)
 {
     if (period > 0) _onTime = period;
 
-    _state.running = false;      // Turn off first to allow restarting in new mode
+    Stop();
     _state.mode = ONE_SHOT_MODE;
     Start();
-}
-
-
-void Blinker::Poll()
-{
-    TRACE(Logger(_classname_, F("Poll")) << endl);
-
-    if (!_state.running) return;
-
-    auto toggleTime = _state.led ? _onTime : _offTime;
-    auto now = millis();
-
-    if ((now - _startTime) >= toggleTime)
-    {
-        Toggle();
-        _startTime = now
-        TRACE(Logger(_classname_, F("Poll")) << F(": LED=") << _state.led << endl);
-
-        if (_state.mode == ONE_SHOT_MODE) Stop();
-    }
 }
--- RTL_Blinker.h
+++ RTL_Blinker.h
@@ -3,16 +3,32 @@
  *
  * created 09 Jul 2016
  * by R. Terry Lessly
- *
  *******************************************************************************/
 #ifndef _RTL_BLINKER_H_
 #define _RTL_BLINKER_H_
 
 #include <inttypes.h>
-#include <IPollable.h>
+#include <RTL_StdLib.h>
+#include <TaskBase.h>
 
 
-class Blinker : public IPollable
+///*****************************************************************************
+/// <summary>
+/// Blinks an LED attached to an Arduino digital pin.
+/// </summary>
+/// <remarks>
+/// The blinker can be configured to be either a repeating or one-shot blinker. 
+/// For a repeating blinker the blink rate is specifed as a total blink period 
+/// (on-time + off-time) and a duty cycle, where the duty cycle specifies
+/// the percentage of the total period (0-100%) that the LED is on.
+/// 
+/// This class is implemented as a Task (subclass of ATask), so it is intended to
+/// work with TaskSchedular singleton. For a Blinker to work, the TaskSchedular::Dispatch()
+/// function must be called very often, such as in every iteration of the loop() 
+/// function.
+/// </remarks>
+///*****************************************************************************
+class Blinker : public TaskBase
 {
     DECLARE_CLASSNAME;
 
@@ -26,6 +42,12 @@ class Blinker : public IPollable
     public: Blinker(uint16_t period) : Blinker(LED_BUILTIN, period, 50) {};
     public: Blinker(uint16_t period, uint16_t dutyCycle) : Blinker(LED_BUILTIN, period, dutyCycle) {};
     public: Blinker(uint8_t ledPin, uint16_t period, uint16_t dutyCycle);
+
+    //**************************************************************************
+    // ATask interface
+    //**************************************************************************
+    public: void Poll() override;
+    public: void StateChanging(TaskState newState) override;
 
     //**************************************************************************
     // Public methods
@@ -43,12 +65,7 @@ class Blinker : public IPollable
     // Public properties
     //**************************************************************************
     public: uint8_t GetMode() { return _state.mode; };
-    public: void SetMode(uint8_t mode) { _state.mode = mode ? ONE_SHOT_MODE : REPEAT_MODE; };
-
-    //**************************************************************************
-    // IPollable interface
-    //**************************************************************************
-    public: void Poll();
+    public: void SetMode(uint8_t mode) { _state.mode = (mode ? ONE_SHOT_MODE : REPEAT_MODE); };
 
     //**************************************************************************
     // Internal state
@@ -56,7 +73,6 @@ class Blinker : public IPollable
     private: struct BlinkerState
     {
         uint8_t pin     : 5;  // The Arduino pin where the LED is connected
-        uint8_t running : 1;  // Running state (0=available, 1=in use)
         uint8_t mode    : 1;  // Blinker mode (0=REPEAT_MODE, 1=ONE_SHOT_MODE);
         uint8_t led     : 1;  // the current state of the LED (1=on, 0=off)
     }
@@ -64,7 +80,7 @@ class Blinker : public IPollable
 
     private: uint16_t _onTime;    // LED on time
     private: uint16_t _offTime;   // LED off time
-    private: uint32_t _startTime; // Start time of last blink period
+    private: uint32_t _timeout;   // timeout for current blink period
 };
 
 //******************************************************************************
@@ -89,10 +105,15 @@ inline void Blinker::Toggle()
 }
 
 
+inline void Blinker::Start()
+{
+    Resume();
+}
+
+
 inline void Blinker::Stop()
 {
-    _state.running = false;
-    Off();  // Ensure the LED off
+    Suspend();
 }
 
 #endif
